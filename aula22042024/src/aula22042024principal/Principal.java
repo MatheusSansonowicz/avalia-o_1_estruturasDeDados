04a44d2fe04b4daea127a1186eeb03df8731a779
MatheusSansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2024-04-22 21:06:56 -0300

    Update Principal.java

--- aula22042024/src/aula22042024principal/Principal.java
+++ aula22042024/src/aula22042024principal/Principal.java
@@ -3,50 +3,58 @@ package aula22042024principal;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.HashSet;
 
 public class Principal {
-	
-	
-	
-	public static void main(String[] args) {
-		
-		Scanner teclado = new Scanner(System.in);
-		
-		private static Set<Atleta> conjuntoAtletas = new HashSet<>();
-		
-		System.out.println("-----MENU-----");
-		System.out.println("1-LISTAR ATLETAS.");
-		System.out.println("2-CADASTRAR ATLETA.");
-		System.out.println("3-PROCURAR POR ATLETA.");
-		System.out.println("3-REMOVER ATLETA.");
-		System.out.println("\n Opcao: ");
-		int opcao = teclado.nextInt();
-		teclado.nextLine();
-		
-		switch(opcao) {
-		case 1:
-			Listar();
-			break;
-		case 2: 
-			Cadastrar();
-			break;
-		case 3:
-			Find();
-			break;
-		case 4:
-			Remover();
-			break;
-		default:
-			System.out.println("Digite uma opcao válida!");
-			break;
-		}
-	}
+    
+    private static List<Atleta> listaAtletas = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        try {
+        System.out.println("-----MENU-----");
+        System.out.println("1-LISTAR ATLETAS.");
+        System.out.println("2-CADASTRAR ATLETA.");
+        System.out.println("3-PROCURAR POR ATLETA.");
+        System.out.println("3-REMOVER ATLETA.");
+        System.out.println("\n Opcao: ");
+        int opcao = Integer.parseInt(teclado.nextLine());
+        teclado.nextLine();
+        
+        switch(opcao) {
+            case 1:
+                Listar();
+                break;
+            case 2: 
+                Cadastrar();
+                break;
+            case 3:
+                Find();
+                break;
+            case 4:
+                Remover();
+                break;
+            default:
+            	 throw new NumberFormatException();
+        }
+        } catch (numberFormatException e) {
+        	System.out.println("Digite uma opcao válida!");
+        }
+    }
+  
 
-	private static void Listar() {
-		// TODO Auto-generated method stub
-		
-	}
+    public static void Listar() {
+        if (listaAtletas.isEmpty()) {
+            System.out.println("Não há atletas cadastrados.");
+        } else {
+            System.out.println("Lista de Atletas:");
+            for (Atleta atleta : listaAtletas) {
+                System.out.println(atleta.Nome + " Telefone: " + atleta.Fone + " Pontuação total: " + atleta.pontuacaoAcumulada); 
+            }
+        }
+    }
+    
+    
+
 }
